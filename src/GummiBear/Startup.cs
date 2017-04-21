f307f3d765c26fa4b19087cb808a7a8ebae5bbcf
Caitlin Hines <caitlinhines@me.com>
2017-04-21 08:40:15 -0700

    include gummi bear kingdom index page and controller

--- src/GummiBear/Startup.cs
+++ src/GummiBear/Startup.cs
@@ -16,17 +16,16 @@ namespace GummiBear
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app)
         {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
+            app.UseMvc(routes =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {
