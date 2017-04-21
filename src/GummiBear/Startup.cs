17ae0ed5590bfc84c9b3a6634ef94adfafeb285c
Caitlin Hines <caitlinhines@me.com>
2017-04-21 09:48:47 -0700

    add dummy bears to the startup so they populate with migration

--- src/GummiBear/Startup.cs
+++ src/GummiBear/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using GummiBear.Models;
-
+using System;
 
 namespace GummiBear
 {
@@ -22,6 +22,10 @@ namespace GummiBear
         }
         public void ConfigureServices(IServiceCollection services)
         {
+            //for testing later, can set up a separate in-memory context(NOT TravelBlogContext)
+            //services.AddDbContext<TravelBlogContext>(opt => opt.UseInMemoryDatabase());
+            services.AddMvc();
+
             services.AddEntityFramework()
                 .AddDbContext<GummiBearDbContext>(options =>
                     options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
@@ -30,12 +34,46 @@ namespace GummiBear
 
         public void Configure(IApplicationBuilder app)
         {
+            var context = app.ApplicationServices.GetService<GummiBearDbContext>();
+            AddTestData(context);
 
-
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Places}/{action=index}/{id?}");
             });
+
+            app.UseStaticFiles();
+
+            app.Run(async (context1) =>
+            {
+                await context1.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        private static void AddTestData(GummiBearDbContext context)
+        {
+            var dummyBear = new Models.Bear
+            {
+                Name = "Black Forest Organic Gummi Bears",
+                Cost = 12,
+                Country = "Germany"
+
+            };
+
+            context.Bears.Add(dummyBear);
+
+            var dummyBear2 = new Models.Bear
+            {
+                Name = "Vitacost Gummi Bears",
+                Cost = 10,
+                Country = "USA"
+            };
+
+            context.Bears.Add(dummyBear2);
+
+            context.SaveChanges();
         }
     }
 }
